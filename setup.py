38bade805f5c2e71088cc71bbfd2857b50fc7d64
Philippe Proulx <eeppeliteloop@gmail.com>
2014-11-07 20:28:59 -0500

    Bump minor version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ packages = [
 
 
 setup(name='pytsdl',
-      version=0.5,
+      version=0.6,
       description='TSDL parser implemented entirely in Python 3',
       author='Philippe Proulx',
       author_email='eeppeliteloop@gmail.com',
